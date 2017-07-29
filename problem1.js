80ece2c65f4832480d2e714df22e5bdccd702775
Chris Chapman <cwchapman1@gmail.com>
2017-07-29 10:43:25 +0700

    Add problem1.js file

---
+++ problem1.js
@@ -0,0 +1,15 @@
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+i = 0;
+for (var j = 0; j < 1000; j++) {
+  if (j % 3 === 0) {
+    i = i + j;
+  } else if (j % 5 === 0) {
+    i = i + j;
+  }
+}
+console.log(i);
